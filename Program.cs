2940dc1cf57606cb744e9d60a440e5fd32412943
Roland Grießer <office@rolandgriesser.at>
2019-05-16 15:47:11 +0200

    Created inital dotnet core project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jpeg2000_decoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
